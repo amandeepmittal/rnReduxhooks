d07c1b994023336fab4b84f28d078c95b99d762e
Aman Mittal <amandeepmittal@live.com>
2020-01-07 16:50:31 +0530

    add useState hook

--- src/screens/ViewNotes.js
+++ src/screens/ViewNotes.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Text, FAB } from 'react-native-paper'
+import React, { useState } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import { Text, FAB, List } from 'react-native-paper'
 
 import Header from '../components/Header'
 
 function ViewNotes({ navigation }) {
+  const [notes, setNotes] = useState([])
+
+  const addNote = note => {
+    note.id = notes.length + 1
+    setNotes([...notes, note])
+  }
+
   return (
     <>
       <Header titleText='Simple Note Taker' />
       <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>You do not have any notes</Text>
-        </View>
+        {notes.length === 0 ? (
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>You do not have any notes</Text>
+          </View>
+        ) : (
+          <FlatList
+            data={notes}
+            renderItem={({ item }) => (
+              <List.Item
+                title={item.noteTitle}
+                description={item.noteValue}
+                descriptionNumberOfLines={1}
+                titleStyle={styles.listTitle}
+              />
+            )}
+            keyExtractor={item => item.id.toString()}
+          />
+        )}
         <FAB
           style={styles.fab}
           small
           icon='plus'
           label='Add new note'
-          onPress={() => navigation.navigate('AddNotes')}
+          onPress={() =>
+            navigation.navigate('AddNotes', {
+              addNote
+            })
+          }
         />
       </View>
     </>
@@ -44,6 +70,9 @@ const styles = StyleSheet.create({
     margin: 20,
     right: 0,
     bottom: 10
+  },
+  listTitle: {
+    fontSize: 20
   }
 })
 
