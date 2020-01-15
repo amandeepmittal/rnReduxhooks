5269de95b0ef62fd6c7f6ff6fb5ef568139c0b0f
Aman Mittal <amandeepmittal@live.com>
2020-01-15 16:40:49 +0530

    implement store

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Provider as PaperProvider } from 'react-native-paper'
 import AppNavigator from './src/navigation'
+import { Provider as StoreProvider } from 'react-redux'
+import store from './src/redux/store'
 
 export default function App() {
   return (
-    <PaperProvider>
-      <AppNavigator />
-    </PaperProvider>
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
+    </StoreProvider>
   )
 }
